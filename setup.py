97329e280875ca9fd4e7734e44f7523f43fff81e
Steinthor Palsson <steini90@gmail.com>
2017-05-21 20:17:35 -0400

    0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='giveme',
-    version='0.0.1',
+    version='0.1.0',
     description='Tiny dependency injection framework',
     author='Steinthor Palsson',
     author_email='steini90@gmail.com',
