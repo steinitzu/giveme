1e21fca9445217dda1064ea8cb586ec098907cc1
Steinthor Palsson <steini90@gmail.com>
2017-05-31 12:51:45 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='giveme',
-    version='0.1.0',
+    version='0.2.0',
     description='Tiny dependency injection framework',
     author='Steinthor Palsson',
     author_email='steini90@gmail.com',
