2d84bbdffe68ac9c1551881d100dbd0dfb784214
Steinthor Palsson <steini90@gmail.com>
2017-07-26 11:47:45 -0400

    Version bump to '0.4.0' -> dependency name override

--- giveme/__init__.py
+++ giveme/__init__.py
@@ -1,3 +1,3 @@
 from .core import inject, register, manager
 
-__version__ = '0.3.1'
+__version__ = '0.4.0'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='giveme',
-    version='0.3.1',
+    version='0.4.0',
     description='Tiny dependency injection framework',
     author='Steinthor Palsson',
     author_email='steini90@gmail.com',
